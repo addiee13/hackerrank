11eddbe227f43b4c7238062fae0736115fd62bd0
Adam Dalwai <adamdalwai1@gmail.com>
2024-04-06 21:38:10 -0400

    completed merge_the_tools

--- merge_the_tools.py
+++ merge_the_tools.py
@@ -1,2 +1,9 @@
 def merge_the_tools(string, k):
-    # your code goes here
+    i = 0
+    while i < len(string):
+        substring = string[i:i+k]
+        for char in set(substring):
+            print(char, end="", sep="")
+        print()
+        i += k
+merge_the_tools("addkdbooo",3)
