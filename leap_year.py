41d79454a5e9f8c12e4b4fb943f76882830827ed
Adam Dalwai <adamdalwai1@gmail.com>
2024-04-06 21:58:34 -0400

    completed leap_year

--- leap_year.py
+++ leap_year.py
@@ -1,7 +1,14 @@
+year = int(input("Enter a year: "))
 def is_leap(year):
     leap = False
-    
-    
+    if year % 4 == 0 and year % 100 == 0:
+        if year % 400 == 0:
+            leap = True
+        else:
+            year = False
+    elif year % 4 == 0 and year % 100 != 0:
+        leap = True
+    else:
+        leap = False
     return leap
-
-year = int(input())
+print(is_leap(year))
